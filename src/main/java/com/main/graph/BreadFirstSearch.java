d97bfa7d7ce6ccb4ec2c76719324e12d6a7152a2
Marie-Lise Hamel <marie-lise.hamel@misys.com>
2016-06-17 17:23:40 +0200

    BFS non recursive

--- src/main/java/com/main/graph/BreadFirstSearch.java
+++ src/main/java/com/main/graph/BreadFirstSearch.java
@@ -1,7 +1,10 @@
 package com.main.graph;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 
 /**
  * Created by mlhamel on 6/13/16.
@@ -14,10 +17,46 @@ public class BreadFirstSearch extends Search {
     public BreadFirstSearch(Graph g, int s){
         super(g,s);
         q = new LinkedList<Integer>();
-
+        visited[s] = true;
+        q.add(s);
+        bfs();
     }
 
+    private void bfs(){
+        while(!q.isEmpty()){
+            int cur = q.remove();
+            for(int next:g.adj(cur)){
+                if(!marked(next)){
+                    count++;
+                    visited[next] = true;
+                    q.add(next);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ClassLoader classLoader = BreadFirstSearch.class.getClassLoader();
+        File file = new File(classLoader.getResource("tinyG.txt").getFile());
+
+        try {
+            Graph g = new Graph(new Scanner(file));
+            BreadFirstSearch bfs = new BreadFirstSearch(g,0);
+            System.out.println(g.toString());
+
+            for (int v = 0; v < g.V(); v++)
+                if (bfs.marked(v))
+                    System.out.print(v + " ");
+            System.out.println();
+
+            if(bfs.count() != g.V())
+                System.out.print("Not ");
+            System.out.println("connected");
 
 
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
