17e143dad8c615413e53223a45c3b1a16ab4ffed
Marie-Lise Hamel <marie-lise.hamel@misys.com>
2016-07-05 18:01:48 +0200

    euler 11

--- src/main/java/com/main/sort/MergeSort.java
+++ src/main/java/com/main/sort/MergeSort.java
@@ -3,5 +3,18 @@ package com.main.sort;
 /**
  * Created by marie-lisehamel on 28/06/2016.
  */
-public class MergeSort {
+public class MergeSort extends Sort {
+
+    /**
+     * Rearranges the array in ascending order, using the natural order.
+     * @param a the array to be sorted
+     */
+    public static void sort(Comparable[] a) {
+        if (a == null) return;
+
+        int N = a.length;
+    }
+
+
+
 }
