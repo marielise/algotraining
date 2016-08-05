c1724e512bd583c7276a47e215231ed7cb1e0323
Marie-Lise Hamel <marie-lise.hamel@misys.com>
2016-08-05 15:52:37 +0200

    shellsort

--- src/main/java/com/main/sort/ShellSort.java
+++ src/main/java/com/main/sort/ShellSort.java
@@ -25,4 +25,31 @@ public class ShellSort extends Sort {
         }
         assert isSorted(a);
     }
+/*
+    private static void show(double[] a, int k, String message) {
+        for (int i = 0; i < a.length; i++) {
+            StdDraw.setPenColor(StdDraw.BLACK);
+            StdDraw.line(i, FF*k, i, FF*k + a[i]*(FF-1));
+        }
+        StdDraw.setPenColor(StdDraw.BOOK_RED);
+        StdDraw.text(0, FF*k - 0.3, message);
+    }
+     public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        double[] a = new double[n];
+        for (int i = 0; i < n; i++)
+          a[i] = StdRandom.uniform(0.0, 1.0);
+        int k = (int) Math.round(Math.log(n) / Math.log(3));  // number of h-increment values
+
+        StdDraw.enableDoubleBuffering();
+
+        StdDraw.setCanvasSize(756, 900);
+        StdDraw.setFont(new Font("SansSerif", Font.PLAIN, 9));
+        StdDraw.setXscale(-1, n+1);
+        StdDraw.setYscale(-1, FF*(k+1));
+        StdDraw.setPenRadius(0.005);
+        sort(a);
+        StdDraw.show();
+    }
+    */
 }
